4ed70a454fd266e6f804afaaaea0ee36d168564a
DudeBro249 <appdevdeploy@gmail.com>
2021-05-29 23:58:59 +0530

    :tada: started dart rewrite

--- docrunner/constants/language_color.py
+++
@@ -1,8 +0,0 @@
-import typer
-
-LANGUAGE_TO_COLOR = {
-    "python": typer.colors.GREEN,
-    "javascript": typer.colors.YELLOW,
-    "typescript": typer.colors.BLUE,
-    "dart": typer.colors.BRIGHT_CYAN,
-}
--- docrunner/exceptions/base_exception.py
+++
@@ -1,6 +0,0 @@
-class DocrunnerBaseException(Exception):
-    def get_message(self) -> str:
-        raise NotImplementedError()
-
-    def get_output_color(self) -> str:
-        raise NotImplementedError()
