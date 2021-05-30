f875d822a280195e1deafb35e335165e4173c114
DudeBro249 <appdevdeploy@gmail.com>
2021-05-30 22:23:21 +0530

    :sparkles: feat(docrunner): finish snippet and snippet_options models

---
+++ bin/models/snippet_options.dart
@@ -0,0 +1,35 @@
+class SnippetOptions {
+  SnippetOptions({
+    this.ignore = false,
+    this.filename,
+  });
+
+  bool ignore;
+  String? filename;
+
+  static SnippetOptions fromDecorators(List<String> decorators) {
+    final snippetOptions = SnippetOptions();
+    if (decorators.isEmpty) {
+      return snippetOptions;
+    }
+
+    for (var decorator in decorators) {
+      decorator = decorator.substring(4, decorator.length - 3).trim();
+      if (decorator == 'docrunner.ignore') {
+        snippetOptions.ignore = true;
+      } else if (decorator.contains('docrunner.file_name')) {
+        final expression = RegExp(r'".*"');
+        final filename = expression
+            .allMatches(decorator)
+            .map((match) {
+              return match.group(0)!.replaceAll('"', '');
+            })
+            .toList()
+            .first;
+        snippetOptions.filename = filename;
+      }
+    }
+
+    return snippetOptions;
+  }
+}
