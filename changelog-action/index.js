a704dacd58477a667a2cfaa3ac41df14534fd521
James Butcher <jamesbutcher167@gmail.com>
2021-07-31 01:19:24 +0100

    :construction_worker: Add custom workflow action

---
+++ changelog-action/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { promise: fs } = require('fs');
+
+const main = async () => {
+  const file = core.getInput("file");
+
+  let content = await fs.readFile(file, 'utf8');
+
+  let changelog = content.split("\n\n##")[0];
+
+  core.setOutput('changelog', changelog);
+}
+
+main().catch(err => core.setFailed(err.message));
